0547520a75e5cb07ae113561f98e49e7005be7ab
Krzysztof <qris70@gmail.com>
2021-04-06 21:30:04 +0200

    test

--- src/zmienne/Zmienne.java
+++ src/zmienne/Zmienne.java
@@ -9,6 +9,21 @@ public class Zmienne {
         System.out.println(Short.MIN_VALUE + " " + Short.MAX_VALUE);
         System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
 
+         long zmiennaLong = 1000000000000L;
+         System.out.println(Long.MIN_VALUE + " " + Long.MAX_VALUE);
+
+         float zmiennaFloat = 12.34f;
+        System.out.println("Float: " + zmiennaFloat);
+
+        double zmiennaDouble = 12.3412345555555;
+        System.out.println("Double: " + zmiennaDouble);
+
+        // Pojedyncze znaki
+        char zmiennaChar = 'A';
+        System.out.println("Char: " + zmiennaChar);
+
+        boolean zmiennaBoolean = true;
+        System.out.println("Boolean: " + zmiennaBoolean);
 
     }
 }
---
+++ src/zmienne_dodatek/ZmienneDodatek.java
@@ -0,0 +1,15 @@
+package zmienne_dodatek;
+
+public class ZmienneDodatek {
+    public static void main(String[] args) {
+
+        int nazwaZmiennej = 5;
+        int a;
+        int b = 4;
+
+        int d = 10, e, f, nazwaZmiennejDwa = 12;
+
+        a = 10;
+        nazwaZmiennej = 101;
+    }
+}
