8f6ea98ddfc632e7ba7685e1ea12396b9b15af1b
Krzysztof <qris70@gmail.com>
2021-04-16 09:58:38 +0200

    add ZadaniaTablice

--- src/tablice_wielowymiarowe/TabliceWielowymiarowe.java
+++ src/tablice_wielowymiarowe/TabliceWielowymiarowe.java
@@ -1,7 +1,27 @@
 package tablice_wielowymiarowe;
 
 public class TabliceWielowymiarowe {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        int[][] tablicaWielowymiarowa = new int[10][10];
+        int[][] nowaTablica = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10}
+        };
+        System.out.println("[0][0]: " + nowaTablica[0][0]);
+        System.out.println("[0][1]: " + nowaTablica[0][1]);
+        System.out.println("[0][2]: " + nowaTablica[0][2]);
+        System.out.println("[0][3]: " + nowaTablica[0][3]);
+        System.out.println("[0][4]: " + nowaTablica[0][4]);
+        System.out.println("[1][0]: " + nowaTablica[1][0]);
+        System.out.println("[1][1]: " + nowaTablica[1][1]);
+        System.out.println("[1][2]: " + nowaTablica[1][2]);
+        System.out.println("[1][3]: " + nowaTablica[1][3]);
+        System.out.println("[1][4]: " + nowaTablica[1][4]);
+
+        System.out.println("Rozmiar tablicy: " + nowaTablica.length);
+        System.out.println(nowaTablica[0].length);
+        System.out.println(nowaTablica[1].length);
 
 
     }
---
+++ src/zadania_tablice/ZadaniaTablice.java
@@ -0,0 +1,24 @@
+package zadania_tablice;
+
+public class ZadaniaTablice {
+    public static void main(String[] args){
+      /*
+      1. Uzupełnij tablicę typu int rozmiaru 5 dowolnymi wartościami.
+        Następnie zsumuj wszystkie elementy tej tablicy i wynik wypisz na ekran.
+        Sumowania dokonaj poprzez dodawanie elementów odwołując się do indeksów tablicy.
+       */
+
+
+
+
+
+
+
+
+
+      /*
+        2. Utwórz tablicę dwuwymiarową typu double wymiaru 3x5 i także wypełnij dowolnymi wartościami.
+         Tym razem zsumuj elementy z każdego wiersza osobno i wypisz wynik na ekran.
+       */
+    }
+}
