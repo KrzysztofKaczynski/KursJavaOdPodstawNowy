3838f89c72e6c53dc0c25319451148eb5a36797f
Krzysztof <qris70@gmail.com>
2021-04-22 18:15:43 +0200

    add PetleBreakContinue i PetleTablice.java

---
+++ src/sekacja10_petle_tablice/PetleTablice.java
@@ -0,0 +1,18 @@
+package sekacja10_petle_tablice;
+
+/**
+ * Created by Krzysztof Kaczynski on 17:48 - 22.04.2021.
+ */
+public class PetleTablice {
+    public static void main(String[] args) {
+
+        int[] tab = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            tab[i] = i;
+        }
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println("tab " + i + ": " + tab[i]);
+        }
+    }
+}
---
+++ src/sekcja10_petla_brake_continue/PetlaBreakContinue.java
@@ -0,0 +1,25 @@
+package sekcja10_petla_brake_continue;
+
+/**
+ * Created by Krzysztof Kaczynski on 19:10 - 21.04.2021.
+ */
+public class PetlaBreakContinue {
+    public static void main(String[] args) {
+
+        int zmienna;
+        for (zmienna = 1; zmienna <= 5; zmienna ++) {
+            System.out.println("Przed continue: " + zmienna);
+            if (zmienna == 3) {
+                continue;
+            }
+            System.out.println("Zmienna: " + zmienna);
+        }
+
+        for (zmienna = 1; zmienna <= 5; zmienna ++) {
+            if (zmienna == 3) {
+                break;
+            }
+            System.out.println("Zmienna w drugiej pętli: " + zmienna);
+        }
+    }
+}
