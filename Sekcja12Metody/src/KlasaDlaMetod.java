feb91be641663d2362e8ae731ad63bc5b9f64d35
Krzysztof <qris70@gmail.com>
2021-04-25 15:12:23 +0200

    add KlasaDlaMetod i Metody

--- Sekcja12Metody/src/KlasaDlaMetod.java
+++ Sekcja12Metody/src/KlasaDlaMetod.java
@@ -5,7 +5,53 @@ public class KlasaDlaMetod {
 
     // typZwracany nazwaMetody(parametry) {}
 
-    void metodaKtoraNicNieZwraca(){
+    void metodaKtoraNicNieZwraca() {
         System.out.println("Wartość wypisana z metody");
     }
+
+    void metodaKtoraPrzyjmujeArgument(int liczba, char pojZnak) {
+        // liczba++;
+        System.out.println("Wartość z metody: " + liczba + "" + pojZnak);
+    }
+
+    void nazewnictwoDowolneAleKolejnaMetoda(boolean czyPokazac, int liczba) {
+        if (czyPokazac) {
+            System.out.println("Liczba: " + liczba);
+        } else {
+            System.out.println("Nie pozwala pokazać!");
+        }
+    }
+
+    int dodawanie(int a, int b) {
+        int wynik = a + b;
+        return wynik;
+    }
+
+    double dodajCosPoPrzecinku(double liczbaDouble) {
+        double wynik = liczbaDouble + 0.55;
+        return wynik;
+    }
+
+    boolean zamienWartoscLogiczna(boolean wartoscLogiczna) {
+        return !wartoscLogiczna;
+    }
+
+    int zwrocOdWartosciLogicznej(boolean czyZwrocic, int liczba) {
+        if (czyZwrocic) {
+            return liczba;
+        } else {
+            return 0;
+        }
+    }
+
+    int przykladKolejnej() {
+        return 100;
+    }
+
+    void voidKtoryMialByCosZwrocic(boolean czyZakonczyc) {
+        if (czyZakonczyc) {
+            return;
+        }
+    }
+
 }
--- Sekcja12Metody/src/Metody.java
+++ Sekcja12Metody/src/Metody.java
@@ -2,7 +2,26 @@
  * Created by Krzysztof Kaczynski on 10:04 - 25.04.2021.
  */
 public class Metody {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        KlasaDlaMetod nazwaObiektu = new KlasaDlaMetod();
+        nazwaObiektu.metodaKtoraNicNieZwraca();
+        nazwaObiektu.metodaKtoraPrzyjmujeArgument(5, 'b');
+        nazwaObiektu.nazewnictwoDowolneAleKolejnaMetoda(false, 100);
+        nazwaObiektu.nazewnictwoDowolneAleKolejnaMetoda(true, 100);
+
+        int wynikDodawania = nazwaObiektu.dodawanie(5, 156);
+        System.out.println("Wynik dodawanie: " + wynikDodawania);
+
+        double wynikKolejnejMetody = nazwaObiektu.dodajCosPoPrzecinku(365.1);
+        System.out.println("Wynik dodawania doubla: " + wynikKolejnejMetody);
+
+        System.out.println("Wartość logiczna: " + nazwaObiektu.zamienWartoscLogiczna(true));
+        nazwaObiektu.voidKtoryMialByCosZwrocic(false);
+        int cyfra = nazwaObiektu.przykladKolejnej();
+        System.out.println(cyfra);
+        int zwrote = nazwaObiektu.zwrocOdWartosciLogicznej(true, 15);
+        System.out.println(zwrote);
 
     }
 
